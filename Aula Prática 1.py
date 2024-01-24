6abc7e1935bc5cc38ff3b92e8818d9cb421606b6
IsmaelGamesYT0 <126985370+IsmaelGamesYT0@users.noreply.github.com>
2024-01-24 19:20:00 -0300

    Aula Prática 1.py

---
+++ Aula Prática 1.py
@@ -0,0 +1,47 @@
+print("Olá, Mundo!")
+
+print(7+4)
+
+print("Olá" , 5)
+
+nome = 'Ismael'
+idade = 17
+peso = 53.3
+print(nome, idade, peso)
+
+nome = input ('Qual é seu nome?:')
+idade = input('Quantos anos você tem?:')
+peso = input('Quanto você pesa?:')
+print(nome, idade, peso)
+
+#Desafio 1
+
+#Crie uma script Python que leia o nome de uma pessoa e mostra uma mensagem de boas-vindas
+#de acordo com o valor digitado.
+
+nome = input("Digite seu nome: ")
+print("Olá", nome ,"Prazer em te conhecer!")
+
+
+#Desafio 2
+
+#Crie um script Python que leia o dia,o mês e o ano de nascimento de uma pessoa e
+#mostra uma mensagem com a data formatada.
+
+dia = input("Dia: ")
+mes = input("Mês: ")
+ano = input("Ano: ")
+print("Você nasceu no dia", dia , "de" , mes , "de", ano , ". Correto?")
+
+
+#Desafio 3
+
+#Crie um script Python que leia dois números e tenta mostrar a soma entre eles.
+
+n1 =int( input("Digite um número: ") )
+
+n2 =int( input("Digite outro número: ") )
+
+print("A soma é:", n1 + n2)
+
+
