66afaf9f0de14f7991e0e630bcd4d58e5e2da7a9
IsmaelGamesYT0 <126985370+IsmaelGamesYT0@users.noreply.github.com>
2024-10-11 13:30:48 -0300

    Primeiros Códigos em Python

---
+++ desafio1.py
@@ -0,0 +1,5 @@
+#Crie um script Python que leia o nome de uma pessoa,
+#e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+
+nome = input('Porfavor, Digite seu nome:')
+print('Seja Bem-vindo!', nome)
---
+++ desafio3.py
@@ -0,0 +1,6 @@
+#Crie um script Python que leia dois números e,
+#tente mostrar a soma entre eles.
+
+num1 = int(input('Digite o primeiro número:'))
+num2 = int(input('Digite o segundo número:'))
+print('A soma entre:', num1, 'e',  num2, 'é igual à:',  num1 + num2 )
